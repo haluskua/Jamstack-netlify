4637267b54477daa652678b1e0238601e7cc9138
haluskua <haluskua.one@gmail.com>
2022-01-26 02:49:31 +1300

    display data to front end through app component in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 //Grab all of the links
 //display all of the links
 //add delete and archive functionality
-
+import LinkList from "./components/LinkList";
 
 function App() {
-  const loadLinks = async() => {
-   const res = await fetch('/api/getLinks')
-   const links = await res.json()
-   console.log(links)
- }  
-  
+  //to keep track of state we mush use useState
+  const [links, setLinks] = useState([]);
+
+  const loadLinks = async () => {
+    try {
+      const res = await fetch("/api/getLinks");
+      const links = await res.json();
+      setLinks(links);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
-    loadLinks()
-  }, [])
+    loadLinks();
+  }, []);
   return (
     <div className="container py-5">
-    <h1 className="text-center mb-5">List 0'</h1>
-      
+      <h1 className="text-center mb-5">List 0'</h1>
+      <LinkList links={links} />
     </div>
   );
 }
